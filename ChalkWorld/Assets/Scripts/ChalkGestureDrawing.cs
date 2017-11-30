cb916fec8cc64019f52dce31cc32707547404d78
Chloe Snyder <csny@seas.upenn.edu>
2017-11-30 18:00:56 -0500

    scene updates

--- ChalkWorld/Assets/Scripts/ChalkGestureDrawing.cs
+++ ChalkWorld/Assets/Scripts/ChalkGestureDrawing.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 using AirSig;
 
-public class DeveloperDefined : BasedGestureHandle {
+public class ChalkGestureDrawing : BasedGestureHandle
+{
 
     // Callback for receiving signature/gesture progression or identification results
     AirSigManager.OnDeveloperDefinedMatch developerDefined;
@@ -12,17 +14,19 @@ public class DeveloperDefined : BasedGestureHandle {
     // gestureId - a serial number
     // gesture - gesture matched or null if no match. Only guesture in SetDeveloperDefinedTarget range will be verified against
     // score - the confidence level of this identification. Above 1 is generally considered a match
-    void HandleOnDeveloperDefinedMatch(long gestureId, string gesture, float score) {
+    void HandleOnDeveloperDefinedMatch(long gestureId, string gesture, float score)
+    {
         textToUpdate = string.Format("<color=cyan>Gesture Match: {0} Score: {1}</color>", gesture.Trim(), score);
     }
 
     // Use this for initialization
-    void Awake() {
+    void Awake()
+    {
         Application.SetStackTraceLogType(LogType.Log, StackTraceLogType.None);
 
         // Update the display text
         //textMode.text = string.Format("Mode: {0}", AirSigManager.Mode.DeveloperDefined.ToString());
-       // textResult.text = defaultResultText = "Pressing trigger and write symbol in the air\nReleasing trigger when finish";
+        // textResult.text = defaultResultText = "Pressing trigger and write symbol in the air\nReleasing trigger when finish";
         //textResult.alignment = TextAnchor.UpperCenter;
         instruction.SetActive(false);
         ToggleGestureImage("All");
@@ -31,7 +35,7 @@ public class DeveloperDefined : BasedGestureHandle {
         developerDefined = new AirSigManager.OnDeveloperDefinedMatch(HandleOnDeveloperDefinedMatch);
         airsigManager.onDeveloperDefinedMatch += developerDefined;
         airsigManager.SetMode(AirSigManager.Mode.DeveloperDefined);
-        airsigManager.SetDeveloperDefinedTarget(new List<string> { "Circle", "Square"});
+        airsigManager.SetDeveloperDefinedTarget(new List<string> { "Circle", "Square" });
         airsigManager.SetClassifier("SquareCircle", "");
 
         checkDbExist();
@@ -50,12 +54,14 @@ public class DeveloperDefined : BasedGestureHandle {
     }
 
 
-    void OnDestroy() {
+    void OnDestroy()
+    {
         // Unregistering callback
         airsigManager.onDeveloperDefinedMatch -= developerDefined;
     }
 
-    void Update() {
+    void Update()
+    {
         UpdateUIandHandleControl();
     }
 }
