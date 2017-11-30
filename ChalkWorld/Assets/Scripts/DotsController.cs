ce06a74401ca6d7082a360edf8c5ded14907dca2
Chloe Snyder <csny@seas.upenn.edu>
2017-11-30 18:22:29 -0500

    convert mouse down to the trigger

--- ChalkWorld/Assets/Scripts/DotsController.cs
+++ ChalkWorld/Assets/Scripts/DotsController.cs
@@ -5,14 +5,58 @@ using UnityEngine;
 public class DotsController : MonoBehaviour {
 
     // Use this for initialization
-   
+	private SteamVR_TrackedController _controller;
+
 	void Start () {
         GameObject obj = GameObject.Find("GlobalObject");
         Global  g = obj.GetComponent<Global>();
 
     }
 
-    void OnMouseDown()
+	private void OnEnable()
+	{
+		_controller = GetComponent<SteamVR_TrackedController>();
+		_controller.TriggerClicked += HandleTriggerClicked;
+		//_controller.PadClicked += HandlePadClicked;
+	}
+
+	private void OnDisable()
+	{
+		_controller.TriggerClicked -= HandleTriggerClicked;
+		//_controller.PadClicked -= HandlePadClicked;
+	}
+
+
+	#region Primitive Spawning
+	private void HandleTriggerClicked(object sender, ClickedEventArgs e)
+	{
+		// this object was clicked - do something
+		GameObject obj = GameObject.Find("GlobalObject");
+		Global g = obj.GetComponent<Global>();
+		bool status = g.selected;
+		Debug.Log("mouse down once");
+		if (status == false)
+		{
+			Debug.Log("status false");
+			g.selected = true;
+			g.start = gameObject.transform.position;
+			// Destroy(gameObject);
+		}
+		else
+		{
+			Debug.Log("status true");
+			g.end = gameObject.transform.position;
+			g.selected = false;
+			g.color = new Color(0.2f, 1, 0.4f);
+			g.DrawLine(g.start, g.end, g.color);
+			Debug.Log("start is" + g.start + "and end is " + g.end);
+			// Destroy(gameObject);
+		}
+	}
+	#endregion
+
+
+  /*  void OnMouseDown()
     {
         // this object was clicked - do something
         GameObject obj = GameObject.Find("GlobalObject");
@@ -36,7 +80,7 @@ public class DotsController : MonoBehaviour {
             Debug.Log("start is" + g.start + "and end is " + g.end);
            // Destroy(gameObject);
         }
-    }
+    }*/
     // Update is called once per frame
     void Update () {
 		
