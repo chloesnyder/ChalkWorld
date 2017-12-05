bc546b48428b7220f5b992948232a057942582ab
Chloe Snyder <csny@seas.upenn.edu>
2017-12-04 22:00:12 -0500

    menu beginning to work

--- ChalkWorld/Assets/Scripts/MenuScript.cs
+++ ChalkWorld/Assets/Scripts/MenuScript.cs
@@ -9,18 +9,22 @@ public class MenuScript : MonoBehaviour {
 	public VRTK_ControllerReference rightController;
 	public GameObject menu;
 	public VRTK_Pointer pointer;
+	bool turnOn;
+	bool pressedOnce;
 
 	// Use this for initialization
 	void Start () {
-		
+		turnOn = false;
+		pressedOnce = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		// toggles menu on and off on touchpad press
 		bool active = pointer.IsActivationButtonPressed ();
-		if (active){
-			menu.SetActive (!menu.activeInHierarchy);
-		}
+		menu.SetActive (active);
+
+
 	}
 
 
