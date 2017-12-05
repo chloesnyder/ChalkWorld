da44440b18eb56bf63d61dd1ac38222087b4fc49
Chloe Snyder <csny@seas.upenn.edu>
2017-12-04 21:43:37 -0500

    click touch pad to bring up menu

---
+++ ChalkWorld/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class MenuScript : MonoBehaviour {
+
+	public VRTK_ControllerReference leftController;
+	public VRTK_ControllerReference rightController;
+	public GameObject menu;
+	public VRTK_Pointer pointer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool active = pointer.IsActivationButtonPressed ();
+		if (active){
+			menu.SetActive (!menu.activeInHierarchy);
+		}
+	}
+
+
+}
+
+
