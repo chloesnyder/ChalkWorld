5a82965db8be49b4adea06f077e78f582e0e42c8
Chloe Snyder <csny@seas.upenn.edu>
2017-12-06 19:50:51 -0500

    menu toggles on and off, selection not implemented

--- ChalkWorld/Assets/Scripts/MenuScript.cs
+++ ChalkWorld/Assets/Scripts/MenuScript.cs
@@ -3,29 +3,82 @@ using System.Collections.Generic;
 using UnityEngine;
 using VRTK;
 
-public class MenuScript : MonoBehaviour {
+public class MenuScript : MonoBehaviour { 
 
-	public VRTK_ControllerReference leftController;
 	public VRTK_ControllerReference rightController;
+    public VRTK_ControllerEvents controlEvents;
 	public GameObject menu;
 	public VRTK_Pointer pointer;
-	bool turnOn;
-	bool pressedOnce;
+
+
+    public float numCubes;
+    public float numCylinders;
+    public bool eraseMode;
+
+    public bool canDrawCube;
+    public bool canDrawCylinder;
+
+    bool menuActive;
+
+
 
 	// Use this for initialization
 	void Start () {
-		turnOn = false;
-		pressedOnce = false;
+        menuActive = false;
+        numCubes = 5;
+        numCylinders = 5;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		// toggles menu on and off on touchpad press
-		bool active = pointer.IsActivationButtonPressed ();
-		menu.SetActive (active);
+
+    void Update()
+    {
+        menu.SetActive(menuActive);
+        pointer.enabled = menuActive;
+        pointer.activateOnEnable = true;
+
+        pointer.pointerRenderer.enabled = menuActive;
 
 
+    }
+
+ 
+    // Update is called once per frame
+    void FixedUpdate () {
+        // toggles menu on and off on touchpad press
+ 
+
+        if(controlEvents.touchpadPressed)
+        {
+            menuActive = !menuActive;
+        }
+  
+
 	}
+
+
+    public void cubePressed()
+    {
+        // enables drawing a cube, takes a cube out of inventory
+        numCubes--;
+        canDrawCube = true;
+        canDrawCylinder = false;
+    }
+
+    public void cylinderPressed()
+    {
+        // enables drawing a cube, takes a cube out of inventory
+        numCylinders--;
+        canDrawCube = false;
+        canDrawCylinder = true;
+    }
+
+    public void eraseModeToggled(bool val)
+    {
+        eraseMode = val;
+    }
+
+
+
+
 
 
 }
